89828a1ba4be785934368ce913a968e6e14b28c0
Janis Rasins <janis.rasins@accenture.com>
2024-12-16 16:08:14 +0200

    Add storybook-host lib

---
+++ libs/storybook-host/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'storybook-host',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/storybook-host',
+};
